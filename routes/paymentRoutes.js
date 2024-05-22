2343fb520a3c929041dc74cabe0a83970c477b72
zsthedev <zsdev166@gmail.com>
2024-05-22 22:53:12 +0500

    Cart Checout Added

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
-import { createCheckOutSession } from '../controllers/paymentController.js';
+import { createCartCheckout, createCheckOutSession } from '../controllers/paymentController.js';
 import { isAuthenticated } from '../middlewares/auth.js';
 const router = express.Router();
 
 router.post('/create_checkout/:id', isAuthenticated, createCheckOutSession)
 
-router.post('/create_cart_checkout', isAuthenticated, createCheckOutSession)
+router.post('/create_cart_checkout', isAuthenticated, createCartCheckout)
 
 export default router
